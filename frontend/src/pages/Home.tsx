b948f182a7dab6e3e28f658f0e5ed33bb6cd0ae5
subash <imsuubash@gmail.com>
2022-12-22 15:02:02 +0545

    add view all categories card

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,6 +10,12 @@ export const Home = () => {
         {categories.map((category) => (
           <Category key={category.id} {...category} />
         ))}
+        <div className="min-w-[180px] relative h-24 cursor-pointer group overflow-hidden rounded-sm select-none bg-dark-tertiary">
+          <div className="absolute h-full w-full bg-gradient-to-t from-dark-secondary via-dark-secondary/0 to-transparent rounded-sm"></div>
+          <p className="absolute bottom-2 left-2 font-bold text-accent text-sm">
+            Show all categories
+          </p>
+        </div>
       </div>
       <div className="pt-10 padding-custom-x grid grid-cols-2 gap-2">
         {products.map((product) => (
