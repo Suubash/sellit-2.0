ee111ed1177b856d6026275dd894ba22571fbf12
subash <imsuubash@gmail.com>
2022-12-22 21:56:28 +0545

    working footer links

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -1,3 +1,4 @@
+import { Link, useLocation } from "react-router-dom";
 import {
   HomeIcon,
   MagnifyingGlassIcon,
@@ -6,25 +7,30 @@ import {
 } from "@heroicons/react/24/outline";
 
 export const Footer = () => {
+  const location = useLocation();
+
+  const footer_links = [
+    { title: "Home", icon: <HomeIcon />, to: "/" },
+    { title: "Search", icon: <MagnifyingGlassIcon />, to: "/search" },
+    { title: "Notif.", icon: <BellIcon />, to: "/notification" },
+    { title: "Profile", icon: <UserCircleIcon />, to: "/profile" },
+  ];
+
   return (
     <footer className="text-light fixed bottom-0 w-full bg-gradient-to-b from-dark-secondary/70 to-dark-secondary backdrop-blur-sm">
       <div className="py-3 px-12 flex items-center justify-between">
-        <div className="text-xs font-semibold flex flex-col items-center">
-          <HomeIcon className="w-8" />
-          <span className="mt-1">Home</span>
-        </div>
-        <div className="text-xs font-semibold flex flex-col items-center">
-          <MagnifyingGlassIcon className="w-8" />
-          <span className="mt-1">Search</span>
-        </div>
-        <div className="text-xs font-semibold flex flex-col items-center">
-          <BellIcon className="w-8" />
-          <span className="mt-1">Notif.</span>
-        </div>
-        <div className="text-xs font-semibold flex flex-col items-center">
-          <UserCircleIcon className="w-8" />
-          <span className="mt-1">Profile</span>
-        </div>
+        {footer_links.map((link) => (
+          <Link
+            to={link.to}
+            key={link.title}
+            className={`text-xs font-semibold flex flex-col items-center ${
+              location.pathname === link.to && "text-accent"
+            }`}
+          >
+            <span className="w-8">{link.icon}</span>
+            <span className="mt-1">{link.title}</span>
+          </Link>
+        ))}
       </div>
     </footer>
   );
