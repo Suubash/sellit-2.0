343c6acd5978cc587d98f57e57a12402b06590c4
subash <imsuubash@gmail.com>
2023-01-11 19:25:26 +0545

    add Link navigation

--- frontend/src/components/shared/Navbar.tsx
+++ frontend/src/components/shared/Navbar.tsx
@@ -1,6 +1,6 @@
 import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 import { useAppSelector } from "../../hooks/reduxHook";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { RootState } from "../../redux/store";
 
 export const Navbar = () => {
@@ -12,19 +12,19 @@ export const Navbar = () => {
     <header className="w-full bg-gradient-to-b from-dark to-dark-secondary">
       <div className="flex items-center justify-between padding-custom">
         <div>
-          <p className="font-semibold text-light select-none cursor-pointer text-2xl tracking-tight leading-tight">
+          <Link
+            to={"/"}
+            className="font-semibold text-light select-none cursor-pointer text-2xl tracking-tight leading-tight"
+          >
             Sellit 2.0
-          </p>
+          </Link>
         </div>
-        <div
-          onClick={() => navigate("/cart")}
-          className="flex items-center gap-3 relative"
-        >
+        <Link to={"/cart"} className="flex items-center gap-3 relative">
           <ShoppingCartIcon className="icon-solid" />
           <div className="absolute -right-1 select-none -top-2 text-xs font-semibold bg-accent rounded-full h-5 w-5 flex items-center justify-center">
             {all_cart_items.length}
           </div>
-        </div>
+        </Link>
       </div>
     </header>
   );
